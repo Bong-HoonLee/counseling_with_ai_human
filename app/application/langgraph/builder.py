7d8140dfe9af3a371490021927ffe55473dc06d4
Bong-HoonLee <115579916+Bong-HoonLee@users.noreply.github.com>
2025-09-11 17:54:38 +0900

    feat: app/application

---
+++ app/application/langgraph/builder.py
@@ -0,0 +1,27 @@
+def build_graph():
+    from langgraph.graph import START, END, StateGraph
+    from .node import GraphState, Chatbot_node
+    
+    workflow = StateGraph(GraphState)
+    chatbot_node = Chatbot_node("openai_gpt")
+
+    # add node
+    workflow.add_node("retrieve", chatbot_node.retrieve)
+    workflow.add_node("rag_response", chatbot_node.rag_response)
+    workflow.add_node("gpt_relevance_check", chatbot_node.relevance_check)
+    workflow.add_node("conversation_response", chatbot_node.conversation_response)
+    
+    # add edge
+    workflow.add_edge(START, "retrieve")
+    # workflow.add_edge("rag_response", "gpt_relevance_check")
+    # workflow.add_edge("gpt_relevance_check", END)
+    workflow.add_conditional_edges(
+        source="retrieve",
+        path=chatbot_node.is_retrieve,
+        path_map={"rag": "rag_response", 'conversation': 'conversation_response'},
+    )
+    
+    workflow.add_edge("rag_response", END)
+    workflow.add_edge("conversation_response", END)
+    
+    return workflow
