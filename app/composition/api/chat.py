18e194f4177daf27ea1c6ab27f4034e6970b135f
Bong-HoonLee <115579916+Bong-HoonLee@users.noreply.github.com>
2025-09-11 17:54:58 +0900

    feat: app/composition/

---
+++ app/composition/api/chat.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from app.core import Chatbot_node, build_graph, GraphState
+from langgraph.checkpoint.memory import MemorySaver
+
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.models import ChatRequest, ChatResponse
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",   # React 개발 서버
+    # "https://your-domain.com"  # 배포 시 프론트엔드 도메인
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+memory = MemorySaver()
+workflow = build_graph()
+
+# compiled_graph = workflow.compile(checkpointer=memory)
+compiled_graph = workflow.compile()
+
+@app.post("/chat", response_model=ChatResponse)
+async def chat_endpoint(chat_request: ChatRequest):
+    
+    user_message = chat_request.message
+    input = GraphState(question=[("user",  user_message)])
+    
+    
+    output = await compiled_graph.ainvoke(input)
+                
+    return ChatResponse(reply=output)
